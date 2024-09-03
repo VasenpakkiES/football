12a2641cce9982689c31be179ddac7f73f140219
Eepi <59473155+VasenpakkiES@users.noreply.github.com>
2024-09-03 18:41:47 +0300

    Eero Siljander's Omnia course files. Uploaded 3rd September 2024.
    Eero Siljander's Omnia course files. Uploaded 3rd September 2024.

---
+++ currency3.js
@@ -0,0 +1,34 @@
+https://api.boffsaopendata.fi/referencerates/api/ExchangeRate[?type][&startDate][&endDate][&currencies][&includeNonECBPublCur][&asXml]
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+
+        // Send a GET request to the URL
+        fetch('https://api.boffsaopendata.fi/referencerates/api/ExchangeRate[?type][&startDate][&endDate][][&includeNonECBPublCur][False]')
+        // Put response into json form
+        .then(response => response.json())
+        .then(data => {
+            // Get currency from user input and convert to upper case
+            const currency = document.querySelector('#currency').value.toUpperCase();
+
+            // Get rate from data
+            const rate = data.rates[currency];
+
+            // Check if currency is valid:
+            if (rate !== undefined) {
+                // Display exchange on the screen
+                document.querySelector('#result').innerHTML = `1 EURO is equal to ${rate.toFixed(1)} ${currency}.`;
+            }
+            else {
+                // Display error on the screen
+                document.querySelector('#result').innerHTML = 'Invalid Currency.';
+            }
+        })
+        // Catch any errors and log them to the console
+        .catch(error => {
+            console.log('Error:', error);
+        });
+        // Prevent default submission
+        return false;
+    }
+});
---
+++ currency5.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+        fetch('https://api.boffsaopendata.fi/referencerates/api/ExchangeRate', {
+            method: 'GET',
+            // Request headers
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Ocp-Apim-Subscription-Key': 'Esiljand0607?!',}
+        })
+        /* Send a GET request to the URL
+       fetch('https://api.boffsaopendata.fi/referencerates/api/ExchangeRate?eur', {
+        method: 'GET',
+        // Request headers
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Ocp-Apim-Subscription-Key': '',}
+    }) */
+        // Put response into json form
+        .then(response => response.json())
+        .then(data => {
+            // Get currency from user input and convert to upper case
+            const currency = document.querySelector('#Currency').value.toUpperCase();
+
+            // Get rate from data
+            const rate = data.Value[currency];
+
+            // Check if currency is valid:
+            if (rate !== undefined) {
+                // Display exchange on the screen
+                document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}.`;
+            }
+            else {
+                // Display error on the screen
+                document.querySelector('#result').innerHTML = 'Invalid Currency.';
+            }
+        })
+        // Catch any errors and log them to the console
+        .catch(error => {
+            console.log('Error:', error);
+        });
+        // Prevent default submission
+        return false;
+    }
+});
