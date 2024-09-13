d72dc4ff331cf11c2e2ff0f7d6adf88223590005
Eero Siljander <eero.siljander59@gmail.com>
2024-09-13 16:15:28 +0300

    addtocart.php

---
+++ tasks.js
@@ -0,0 +1,68 @@
+// Wait for page to load
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Select the submit button and input to be used later
+    const submit = document.querySelector('#submit');
+    const newTask = document.querySelector('#task');
+
+    // Disable submit button by default:
+    submit.disabled = true;
+
+    // Listen for input to be typed into the input field
+    newTask.onkeyup = () => {
+        if (newTask.value.length > 0) {
+            submit.disabled = false;
+        }
+        else {
+            submit.disabled = true;
+        }
+    }
+
+    // Listen for submission of form
+    document.querySelector('form').onsubmit = () => {
+
+        // Find the task the user just submitted
+        const task = newTask.value;
+
+        // Create a list item for the new task and add the task to it
+        const li = document.createElement('li');
+        li.innerHTML = task;
+
+        // Add new element to our unordered list:
+        document.querySelector('#tasks').append(li);
+
+        // Clear out input field:
+        newTask.value = '';
+
+        // Disable the submit button again:
+        submit.disabled = true;
+
+        // Stop form from submitting
+        return false;
+    }
+});
+
+// Check if there is already a value in local storage
+if (!localStorage.getItem('counter')) {
+
+    // If not, set the counter to 0 in local storage
+    localStorage.setItem('counter', 0);
+}
+            
+function count() {
+    // Retrieve counter value from local storage
+    let counter = localStorage.getItem('counter');
+
+    // update counter
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+
+    // Store counter in local storage
+    localStorage.setItem('counter', counter);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Set heading to the current value inside local storage
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count;
+});
